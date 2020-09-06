6a7543fbdb9ea364b09db320ae198418df9b4b2b
Guy Baranyay <GuyziB@Guys-MacBook-Air.local>
2020-09-06 17:26:39 +0200

    Starting again after a few days off.

--- script/script.js
+++ script/script.js
@@ -6,3 +6,10 @@ function popup(mylink, windowname) {
   window.open(href, windowname, 'height=200, width=400, scrollbars=yes');
   return false;
 }
+
+$(document).ready(function() {
+  $('#header_video_link').magnificPopup({
+    type:'inline',
+    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  });
+});
